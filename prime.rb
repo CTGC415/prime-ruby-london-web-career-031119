1ae22a4b5478b190dcee874c603059048ce8721e
Cully Curwen <c.t.g.curwen@gmail.com>
2019-03-03 16:59:31 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -6,9 +6,9 @@ def prime?(num)
   range = [1..(num - 1)]
   range.shift
   range.each do |x|
-    if ((num % x) == 0 )
+    if num % x == 0
       return false
     end
   end
-  return true
+  true
 end
